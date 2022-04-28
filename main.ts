f65e86a16d764ca7e0fa754b06a48acac0b910d0
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-04-28 08:25:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
 function Countdown () {
     for (let index = 0; index <= 2; index++) {
         basic.showNumber(3 - index)
+        music.playTone(262, music.beat(BeatFraction.Quarter))
     }
     music.playTone(392, music.beat(BeatFraction.Whole))
     basic.showString("GO!")
